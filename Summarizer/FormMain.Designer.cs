0558151c68a1a2686a82edeac903262f0d300f46
chowizard <chowizard@hotmail.com>
2024-11-07 00:45:21 +0900

    add : [Summarizer] Add project files.

---
+++ Summarizer/FormMain.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Summarizer
+{
+    partial class Summarizer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonToConvert = new Button();
+            textBoxInput = new TextBox();
+            textBoxOutput = new TextBox();
+            labelInput = new Label();
+            labelOutput = new Label();
+            buttonClear = new Button();
+            buttonCopyOutput = new Button();
+            SuspendLayout();
+            // 
+            // buttonToConvert
+            // 
+            buttonToConvert.Location = new Point(343, 121);
+            buttonToConvert.Name = "buttonToConvert";
+            buttonToConvert.Size = new Size(125, 33);
+            buttonToConvert.TabIndex = 0;
+            buttonToConvert.Text = "변환하기";
+            buttonToConvert.UseVisualStyleBackColor = true;
+            buttonToConvert.Click += ButtonConvert_Click;
+            // 
+            // textBoxInput
+            // 
+            textBoxInput.Location = new Point(33, 34);
+            textBoxInput.Multiline = true;
+            textBoxInput.Name = "textBoxInput";
+            textBoxInput.Size = new Size(279, 359);
+            textBoxInput.TabIndex = 1;
+            // 
+            // textBoxOutput
+            // 
+            textBoxOutput.Location = new Point(498, 34);
+            textBoxOutput.Multiline = true;
+            textBoxOutput.Name = "textBoxOutput";
+            textBoxOutput.Size = new Size(281, 359);
+            textBoxOutput.TabIndex = 2;
+            // 
+            // labelInput
+            // 
+            labelInput.AutoSize = true;
+            labelInput.Location = new Point(33, 16);
+            labelInput.Name = "labelInput";
+            labelInput.Size = new Size(111, 15);
+            labelInput.TabIndex = 3;
+            labelInput.Text = "변환할 텍스트 입력";
+            // 
+            // labelOutput
+            // 
+            labelOutput.AutoSize = true;
+            labelOutput.Location = new Point(498, 16);
+            labelOutput.Name = "labelOutput";
+            labelOutput.Size = new Size(59, 15);
+            labelOutput.TabIndex = 4;
+            labelOutput.Text = "변환 결과";
+            // 
+            // buttonClear
+            // 
+            buttonClear.Location = new Point(343, 270);
+            buttonClear.Name = "buttonClear";
+            buttonClear.Size = new Size(125, 33);
+            buttonClear.TabIndex = 5;
+            buttonClear.Text = "비우기";
+            buttonClear.UseVisualStyleBackColor = true;
+            buttonClear.Click += buttonClear_Click;
+            // 
+            // buttonCopyOutput
+            // 
+            buttonCopyOutput.Location = new Point(343, 172);
+            buttonCopyOutput.Name = "buttonCopyOutput";
+            buttonCopyOutput.Size = new Size(125, 33);
+            buttonCopyOutput.TabIndex = 6;
+            buttonCopyOutput.Text = "복사하기";
+            buttonCopyOutput.UseVisualStyleBackColor = true;
+            buttonCopyOutput.Click += buttonCopyOutput_Click;
+            // 
+            // Summarizer
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(810, 419);
+            Controls.Add(buttonCopyOutput);
+            Controls.Add(buttonClear);
+            Controls.Add(labelOutput);
+            Controls.Add(labelInput);
+            Controls.Add(textBoxOutput);
+            Controls.Add(textBoxInput);
+            Controls.Add(buttonToConvert);
+            Margin = new Padding(2);
+            Name = "Summarizer";
+            Text = "Summarizer";
+            Load += Summarizer_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonToConvert;
+        private TextBox textBoxInput;
+        private TextBox textBoxOutput;
+        private Label labelInput;
+        private Label labelOutput;
+        private Button buttonClear;
+        private Button buttonCopyOutput;
+    }
+}
---
+++ Summarizer/FormMain.cs
@@ -0,0 +1,127 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Summarizer
+{
+    public partial class Summarizer : Form
+    {
+        private static bool IsCellPhoneNumber(string text)
+        {
+            return !string.IsNullOrEmpty(text) && PhoneNumberRegex().IsMatch(text);
+        }
+
+        private static string StandardizeCellPhoneNumber(string phoneNumberText)
+        {
+            if (string.IsNullOrEmpty(phoneNumberText))
+                return string.Empty;
+
+            StringBuilder builder = new();
+            for (int index = 0, count = 0; index < phoneNumberText.Length; ++index)
+            {
+                var digit = phoneNumberText[index];
+                if (count is 3 or 8)
+                {
+                    builder.Append('-');
+                    ++count;
+                }
+
+                if ((digit >= '0') && (digit <= '9'))
+                {
+                    builder.Append(digit);
+                    ++count;
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        //[GeneratedRegex("\\b010\\d{8}\\b")]
+        [GeneratedRegex("\\b010.*?\\d{4}.*?\\d{4}\\b")]
+        private static partial Regex PhoneNumberRegex();
+
+
+        public Summarizer()
+        {
+            InitializeComponent();
+        }
+
+        private void Summarizer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 변환하기 버튼 클릭 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonConvert_Click(object sender, EventArgs e)
+        {
+            var converted = ConvertText(textBoxInput.Text);
+            textBoxOutput.Text = converted;
+        }
+
+        /// <summary>
+        /// 비우기 버튼 클릭 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxInput.Text = string.Empty;
+            textBoxOutput.Text = string.Empty;
+        }
+
+        /// <summary>
+        /// 변환 결과 문자를 복사하는 버튼 클릭 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCopyOutput_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBoxOutput.Text);
+        }
+
+        /// <summary>
+        /// 텍스트 변환 처리
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private string ConvertText(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return string.Empty;
+
+            var splitTexts = textBoxInput.Text.Split(Environment.NewLine);
+            if (splitTexts.Length <= 0)
+                return string.Empty;
+
+            StringBuilder builder = new();
+            builder.Append("[ ");
+            for (int index = 0; index < splitTexts.Length; ++index)
+            {
+                var splitText = splitTexts[index];
+                if (string.IsNullOrEmpty(splitText))
+                    continue;
+
+                var currentText = splitText.Trim();
+
+                // 휴대전화번호인 텍스트는 '-' 기호로 구분
+                if (IsCellPhoneNumber(currentText))
+                    currentText = StandardizeCellPhoneNumber(currentText);
+                //{
+                //    var phoneNumber = currentText;
+                //    currentText = $"010-{currentText.Substring(3, 4)}-{currentText.Substring(7, 4)}";
+                //}
+
+                builder.Append(currentText);
+
+                if (index < (splitTexts.Length - 1))
+                    builder.Append(" / ");
+            }
+            builder.Append(" ]");
+
+            return builder.ToString();
+        }
+    }
+}
